1fec13588a95cf43f7741b0f9eeaa5649069ba8a
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-08-11 16:52:00 +0200

    passes!

--- test.nix
+++ test.nix
@@ -33,7 +33,7 @@ nixosTest ({ lib, pkgs, ... }: {
               address = ":80";
             };
           };
-          providers.consul.endpoints = [ "${johnIP}:8500" ];
+          providers.consulCatalog.endpoint.address = "${johnIP}:8500";
         };
       };
     };
@@ -99,28 +99,27 @@ nixosTest ({ lib, pkgs, ... }: {
     john.wait_for_unit("multi-user.target")
     client.wait_for_unit("multi-user.target")
 
+    # Wait for consul to be available on 8300
+    john.wait_for_open_port(8300, addr="${johnIP}")
+    john.wait_for_open_port(8500, addr="${johnIP}")
+    john.require_unit_state("consul.service")
+
     # Require that the specific services are running
     jeff.require_unit_state("traefik.service")
     jared.require_unit_state("simplehttp1.service")
     jared.require_unit_state("simplehttp2.service")
 
-    # Wait for consul to be available on 8500
-    john.wait_for_open_port(8500)
-    john.require_unit_state("consul.service")
+    # Register services in consul
+    john.succeed("consul members -http-addr='http://${johnIP}:8500'")
+    john.succeed("consul services register -name jared-simple-http -address jared -port 8001 -http-addr='http://${johnIP}:8500'")
+    john.succeed("consul services register -name jared-simple-http -address jared -port 8002 -http-addr='http://${johnIP}:8500'")
+    out = john.succeed("consul catalog services -http-addr='http://${johnIP}:8500'")
+    print(out)
 
-    john.succeed("consul members")
-    john.succeed("consul services register -name jared-simple-http -address http://jared -port 8001")
-    john.succeed("consul services register -name jared-simple-http -address http://jared -port 8002")
-    john.succeed("consul catalog services")
-
-    john.require_unit_state("consul.service")
-
-    john.succeed("curl http://localhost:8500")
     client.succeed("curl http://${johnIP}:8500")
-    client.succeed("curl http://john:8500")
     client.succeed("curl http://jared:8001")
     client.succeed("curl http://jared:8002")
-    # client.succeed("curl -H 'Host: simple-http-1.jared' http://jeff")
-    # client.succeed("curl -H 'Host: simple-http-2.jared' http://jeff")
+    client.succeed("curl -H 'Host: jared-simple-http' http://${jeffIP}")
+    client.succeed("curl -H 'Host: jared-simple-http' http://${jeffIP}")
   '';
 })
